e4a25eb8bf2a59a7a7afd7da0a5c217d7cf1e32c
Sathish Kumar <sathishkumar3107@gmail.com>
2017-10-21 02:02:31 +0200

    Initial commit - Movie Service

---
+++ movie-service/src/main/java/com/archospark/movieservice/entities/Movie.java
@@ -0,0 +1,78 @@
+package com.archospark.movieservice.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String description;
+    private Integer releaseYear;
+    private Integer length;
+
+    public Movie() {
+    }
+
+    public Movie(String name, String description, Integer releaseYear, Integer length) {
+        this.name = name;
+        this.description = description;
+        this.releaseYear = releaseYear;
+        this.length = length;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(Integer releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public Integer getLength() {
+        return length;
+    }
+
+    public void setLength(Integer length) {
+        this.length = length;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", releaseYear=" + releaseYear +
+                ", length=" + length +
+                '}';
+    }
+}
