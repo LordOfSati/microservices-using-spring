8a9ce641d6af5d1609af8799caf3fb5cc0feae03
Sathish Kumar <sathishkumar3107@gmail.com>
2017-10-24 21:52:20 +0200

    Added hystrix fallback in edge service

--- movie-service-client/src/main/java/com/archospark/movieserviceclient/controller/MovieClientController.java
+++ movie-service-client/src/main/java/com/archospark/movieserviceclient/controller/MovieClientController.java
@@ -1,6 +1,7 @@
 package com.archospark.movieserviceclient.controller;
 
 import com.archospark.movieserviceclient.model.Movie;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -24,6 +26,7 @@ public class MovieClientController {
     }
 
     @GetMapping(value = "/movies")
+    @HystrixCommand(fallbackMethod = "getMoviesFallback")
     public List<Movie> getMovies() {
         final String serviceUrl = "http://movie-service/movies";
         ParameterizedTypeReference<List<Movie>> parameterizedType = new
@@ -31,5 +34,9 @@ public class MovieClientController {
         ResponseEntity<List<Movie>> response = restTemplate.exchange(serviceUrl, HttpMethod.GET, null,
                 parameterizedType);
         return response.getBody();
+    }
+
+    public List<Movie> getMoviesFallback() {
+        return Collections.emptyList();
     }
 }
